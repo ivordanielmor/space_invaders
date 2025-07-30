153ad4dc7b6b52ece6820bb12bbc969b2d1a5d07
Daniel Mór Ivor <ivordanielmor@gmail.com>
2025-07-30 21:50:52 +0200

    Hozzáadva: Mozgatás balra/jobbra, szélek ellenőrzése

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# 2. Játékos sprite betöltése és kezdeti pozíció
-# Tölts be egy „player.png” nevű képet (sprite-ot),
-# majd állítsd középre a képernyő alján.
+# 3. Mozgatás balra/jobbra, szélek ellenőrzése
+# Figyeld a balra/jobbra nyilakat! Minden frame-ben frissítsd a sprite
+#  pozícióját, és akadályozd meg, hogy kimenjen a képernyő szélén túl.
 
 import pygame
 pygame.init()
@@ -22,12 +22,25 @@ player_rect.midbottom = (WIDTH // 2, HEIGHT)
 clock = pygame.time.Clock()
 running = True
 
+player_speed = 5
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0,0,0))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += player_speed
+
+    if player_rect.left < 0:
+        player_rect.left = 0
+    if player_rect.right > WIDTH:
+        player_rect.right = WIDTH
+
+    screen.fill((0, 0, 0))
     screen.blit(player_image, player_rect)
     pygame.display.flip()
     clock.tick(60)
