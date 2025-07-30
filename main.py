397782bcda36103abb6f4b1daad716ac298758a4
Daniel Mór Ivor <ivordanielmor@gmail.com>
2025-07-30 21:49:57 +0200

    Hozzáadva: Játékos sprite betöltése

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-# 1. Ablak és fő ciklus indítása
-# Elsőként indítsd el a Pygame-et, hozz létre egy 800×600-as ablakot,
-# és egy végtelen game loop-ot, amely minden frame-ben frissíti a képernyőt.
+# 2. Játékos sprite betöltése és kezdeti pozíció
+# Tölts be egy „player.png” nevű képet (sprite-ot),
+# majd állítsd középre a képernyő alján.
 
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+player_image = pygame.image.load("player.png").convert_alpha()
+
+original_width, original_height = player_image.get_size()
+
+scale_factor = 2
+new_size = (original_width * scale_factor, original_height * scale_factor)
+player_image = pygame.transform.smoothscale(player_image, new_size)
+
+player_rect = player_image.get_rect()
+player_rect.midbottom = (WIDTH // 2, HEIGHT)
+
 clock = pygame.time.Clock()
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((100, 200, 200))
+
+    screen.fill((0,0,0))
+    screen.blit(player_image, player_rect)
     pygame.display.flip()
     clock.tick(60)
+
 pygame.quit()
